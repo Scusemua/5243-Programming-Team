f76ba10f553bc6ce9c13b7903d066a55c9a8057c
Ben Carver <benrcarver@gmail.com>
2015-01-29 14:01:06 -0500

    Swag

--- src/org/usfirst/frc/team5243/robot/commands/StrafeCommand.java
+++ src/org/usfirst/frc/team5243/robot/commands/StrafeCommand.java
@@ -46,6 +46,7 @@ public class StrafeCommand extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
+    	System.out.println("Strafing ended");
     }
 
     // Called when another command which requires one or more of the same
